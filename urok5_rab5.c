61987b11fb290aafeaa6ec25487c7e0f8be69f8d
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-06 17:55:40 +0300

    Create urok5_rab5.c

---
+++ urok5_rab5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int count_brackets(char brackets){
+
+}
+
+int main(){
+    char characters;
+    do{
+        characters = getchar();
+        putchar(characters);
+    }while (characters != '.');
+    //for(int i =0; i<)
+    //printf("\n%d", sizeof(characters)/sizeof(characters));
+}

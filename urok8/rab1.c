f02f0219f545f60b17179838510c7cd2e9b7a904
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-27 13:43:44 +0300

    Create rab1.c

---
+++ urok8/rab1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int sort_array(int size, int a[]){
+    int count;
+    for(int i = 0; i<size-1; i++){
+        for(int j = i; j<size; j++){
+            if(a[i] >a[j]){
+                count = a[i];
+                a[i] = a[j];
+                a[j] = count;
+            }
+        }
+    }
+    for(int i = 0; i<size; i++){
+        printf("%d ", a[i]);
+    }
+}
+
+int main(){
+    int mass[] = {20, 19, 4, 3, 2, 1, 18, 17, 13, 12, 11, 16, 15, 14, 10, 9, 8, 7, 6, 5};
+    sort_array(20, mass);
+    printf("Hello world!");
+}

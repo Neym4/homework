52b390754afbb5427c5452c51687107f0fc87b29
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-27 14:16:14 +0300

    Create rab5.c

---
+++ urok8/rab5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int change_max_min(int size, int a[]){
+    int max = 0, min = 0;
+    for(int i = 0; i<size; i++){
+        if(a[i]<a[min]){
+            min = i;
+        }
+        if(a[i]>a[min]){
+            max = i;
+        }
+    }
+    int tmp;
+    tmp = a[min];
+    a[min] = a[max];
+    a[max] = tmp;
+}
+
+int main(){
+    int mass[] = {1,2,3,4,5,6,7,8,9,10};
+    change_max_min(10, mass);
+}

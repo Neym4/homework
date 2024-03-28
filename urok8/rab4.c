072f0fc263d85959e4b00043b11c4cd420ea3461
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-28 11:04:24 +0300

    Update rab4.c

--- urok8/rab4.c
+++ urok8/rab4.c
@@ -5,19 +5,18 @@ int find_max_array(int size, int a[]){
     for(int i = 0; i < size; i++){
         for(int j = i+1; j<size-1; j++){
             if(a[i]==a[j]){
-                printf("Yes");
-                return 0;
+                return 1;
             }
         }
     }
-    printf("no");
+    return 0;
 }
 
 int main(){
     int mass[] = {1,2,1,4,5};
-
-    // for(int i = 0; i<100; i++){
-    //     mass[i] = i;
-    // }
-    find_max_array(100, mass);
+    if(find_max_array(sizeof(mass)/sizeof(mass[0]), mass) == 1){
+        printf("Yes");
+    }else{
+        printf("No");
+    }
 }

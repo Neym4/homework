1d1db6ee92f82203d89b1ed531b3c835a5c95218
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-28 10:57:30 +0300

    Update rab3.c

--- urok8/rab3.c
+++ urok8/rab3.c
@@ -2,19 +2,16 @@
 
 int find_max_array(int size, int a[]){
     int max = a[0];
-    
     for(int i = 0; i < size; i++){
         if(a[i]>max){
             max = a[i];
         }
     }
-    printf("%d ", max);
-    // for(int i = 0; i<size; i++){
-    //     printf("%d ", a[i]);
-    // }
+    return max;
 }
 
 int main(){
     int mass[] = {20, 19, 4, 3, 2, 1, 18, 17, 13, 12, 11, 16, 15, 14, 10, 9, 8, 7, 564, 5};
-    find_max_array(20, mass);
+    printf("%d", find_max_array(20,mass));
+    return 0;
 }

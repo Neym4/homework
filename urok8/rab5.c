6ce9164b54baadc8285a29404ae2fc9b85239182
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-03-28 11:07:26 +0300

    Update rab5.c

--- urok8/rab5.c
+++ urok8/rab5.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int change_max_min(int size, int a[]){
+void change_max_min(int size, int* a){
     int max = 0, min = 0;
     for(int i = 0; i<size; i++){
         if(a[i]<a[min]){
@@ -19,4 +19,8 @@ int change_max_min(int size, int a[]){
 int main(){
     int mass[] = {1,2,3,4,5,6,7,8,9,10};
     change_max_min(10, mass);
+    for(int i =0; i<10; i++){
+        printf("%d ", mass[i]);
+    }
+    return 0;
 }

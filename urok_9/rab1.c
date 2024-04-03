2cc7905f1bec70d29f1da519c5160886f0a05899
Neym4 <47027219+Neym4@users.noreply.github.com>
2024-04-03 18:40:34 +0300

    Create rab1.c

---
+++ urok_9/rab1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *inputFile, *outputFile;
+    char inputString[1001];
+    char outputString[1001];
+    int i = 0;
+    inputFile = fopen("input.txt", "r");
+
+    fscanf(inputFile, "%s", inputString);
+    fclose(inputFile);
+    printf("%s ", inputString);
+    while (inputString[i] != '\0') {
+        printf("%c ", inputString[i]);
+        if (islower(inputString[i])) {
+            outputString[i] = (inputString[i] == 'a') ? 'b' : inputString[i]; 
+        }else if (islower(inputString[i])) {
+            outputString[i] = (inputString[i] == 'b') ? 'a' : inputString[i];
+        } 
+        if (isupper(inputString[i])) {
+            outputString[i] = (inputString[i] == 'A') ? 'B' : inputString[i];
+        }else if (isupper(inputString[i])) {
+            outputString[i] = (inputString[i] == 'B') ? 'A' : inputString[i];
+        }
+
+        i++;
+    }
+
+    outputFile = fopen("output.txt", "w");
+    fprintf(outputFile, "%s\n", outputString);
+    printf("%s", outputString);
+    fclose(outputFile);
+    return 0;
+}
